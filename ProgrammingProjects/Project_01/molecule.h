5aabd413eb7b83b004ab6bfa044c5150c9aef933
47-5 <1197748182@qq.com>
2026-08-13 11:44:45 +0800

    Complete Project 1 molecular analysis

--- ProgrammingProjects/Project_01/molecule.h
+++ ProgrammingProjects/Project_01/molecule.h
@@ -6,9 +6,12 @@
 #define LEARNING_CPP_FOR_CHEMISTRY_MOLECULE_H
 
 #include <cstddef>
+#include <filesystem>
 #include <vector>
-
+#include <string>
 #include <Eigen/Core>
+#include <Eigen/Dense>
+#include <Eigen/Eigenvalues>
 
 struct Atom {
     int atomic_number;
@@ -20,8 +23,32 @@ class Molecule {
 public:
     explicit Molecule(std::vector<Atom> atoms);
 
+    static Molecule from_file(const std::filesystem::path& path);
+
     [[nodiscard]] std::size_t atomCount() const;
-    [[nodiscard]] double distanceBetweenAtoms(std::size_t atom1_index, std::size_t atom2_index) const;
+    [[nodiscard]] const Atom& atom(std::size_t index) const;
+
+    [[nodiscard]] double get_distance_by_ij_index(std::size_t i, std::size_t j) const;
+    void print_distances() const;
+
+    [[nodiscard]] double get_angele_by_ijk_index(std::size_t i, std::size_t j, std::size_t k) const;
+    void print_angles() const;
+
+    [[nodiscard]] double get_out_of_plane_angle_by_ijkl_index(std::size_t i, std::size_t j, std::size_t k, std::size_t l) const;
+    void print_out_of_plane_angles() const;
+
+    [[nodiscard]] double get_torsional_angle_by_ijkl_index(std::size_t i, std::size_t j, std::size_t k, std::size_t l) const;
+    void print_torsional_angles() const;
+
+    [[nodiscard]] Eigen::Vector3d get_center_of_mass() const;
+    [[nodiscard]] Eigen::Matrix3d get_moment_of_inertia_tensor() const;
+    [[nodiscard]] Eigen::Vector3d get_principal_moments_of_inertia() const;
+    [[nodiscard]] std::string get_rotor_type() const;
+
+    [[nodiscard]] Eigen::Vector3d get_principal_moments_amu_angstrom2() const;
+    [[nodiscard]] Eigen::Vector3d get_principal_moments_g_cm2() const;
+    [[nodiscard]] Eigen::Vector3d get_rotational_constants_mhz() const;
+    [[nodiscard]] Eigen::Vector3d get_rotational_constants_cm_inverse() const;
 
 private:
     std::vector<Atom> _atoms;
---
+++ ProgrammingProjects/Project_01/physical_constants.h
@@ -0,0 +1,15 @@
+//
+// Created by lrc on 2026/8/12.
+//
+
+#ifndef LEARNING_CPP_FOR_CHEMISTRY_PHYSICAL_CONSTANTS_H
+#define LEARNING_CPP_FOR_CHEMISTRY_PHYSICAL_CONSTANTS_H
+
+inline constexpr double PI = 3.14159265358979323846;
+inline constexpr double BOHR_TO_ANGSTROM = 0.5291772105;
+inline constexpr double BOHR_TO_CM = BOHR_TO_ANGSTROM * 1e-8;
+inline constexpr double AMU_TO_GRAM = 1.66053906892e-24;
+inline constexpr double PLANCK_CONSTANT_ERG_S = 6.62607015e-27;
+inline constexpr double SPEED_OF_LIGHT_CM_S = 2.99792458e10;
+
+#endif // LEARNING_CPP_FOR_CHEMISTRY_PHYSICAL_CONSTANTS_H
